364af7757cc5d577bc02e2af8baf15ddc34d192e
Fernando <fernandoalcocer812@hotmail.com>
2022-04-10 01:03:28 -0300

    Se añadio funcionalidad al boton pedir carta, y detener

--- 02 - Blackjack/assets/js/juego.js
+++ 02 - Blackjack/assets/js/juego.js
@@ -10,6 +10,16 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'K', 'Q'];
 
+let puntosJugador = 0,
+    puntosComputadora = 0;
+
+// Referencias del HTML
+const btnPedir   = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
+const smalls     = document.querySelectorAll('small');
+const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector('#computadora-cartas');
+
 const crearDeck = () =>{
 for( let i = 2; i <=10; i++){
     for(let tipo of tipos){
@@ -30,14 +40,119 @@ for(let tipo of tipos){
     deck.push(especial + tipo);
     }
 }
-console.log(deck);
+// console.log(deck);
 
 
 deck = _.shuffle(deck);
 
-console.log(deck);
+// console.log(deck);
 
 return deck;
 }
 
 crearDeck();
+
+// Esta funcion me permite tomar una carta
+const pedirCarta = () => {
+
+    if ( deck.lenght === 0 ) {
+        throw 'No hay mas cartas en el deck';
+    }
+    let carta = deck.shift();
+    // console.log(carta);
+    // console.log(deck);
+    return carta;
+}
+
+// pedirCart a();
+const valorCarta = (carta) => { 
+
+    const valor = carta.substring(0, carta.length -1);
+    return ( isNaN(valor) ) ?
+            (valor === 'A' )? 11 : 10
+            : valor * 1;
+    // let puntos = 0;
+    // if ( isNaN( valor ) ) {  //isNaN = is not a number, arroja valor truo o false
+    //    puntos = ( valor === 'A' ) ? 11 : 10;
+    
+    // } else {
+       
+    //     puntos = valor * 1; // al multiplicar el valor numero en forma string por 1, se transforma en un numero
+    
+    // }
+    // console.log({ valor });
+    // console.log( puntos );
+};
+
+// Turno de la computadora
+
+const turnoComputadora = ( puntosMinimos ) => {
+
+   do{
+        const carta = pedirCarta();
+        
+        puntosComputadora = puntosComputadora + valorCarta( carta );   
+        smalls[1].innerText= puntosComputadora;
+        console.log( puntosComputadora );
+
+        // <!-- <img class="carta" src="assets/cartas/2C.png"> -->
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${ carta }.png`;
+        imgCarta.classList.add('carta');
+        divCartasComputadora.append(imgCarta);
+
+            if( puntosMinimos > 21) {
+                break;
+            }
+
+    }while( (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21)){
+
+    };
+
+}
+
+// const valor = valorCarta( pedirCarta() );
+// console.log( {valor} );
+
+// Eventos
+btnPedir.addEventListener('click', () => {
+
+   const carta = pedirCarta();
+   
+   puntosJugador = puntosJugador + valorCarta( carta );
+   
+   
+   smalls[0].innerText= puntosJugador;
+   console.log( puntosJugador );
+
+   // <!-- <img class="carta" src="assets/cartas/2C.png"> -->
+   const imgCarta = document.createElement('img');
+   imgCarta.src = `assets/cartas/${ carta }.png`;
+   imgCarta.classList.add('carta');
+   divCartasJugador.append(imgCarta);
+
+   if( puntosJugador > 21 ) {
+       console.warn('Lo siento mucho, perdiste');
+       btnPedir.disabled = true;
+       btnDetener.disabled = true;
+       turnoComputadora( puntosJugador );
+   }else if( puntosJugador === 21 ){
+       btnPedir.disabled = true;
+       btnDetener.disabled = true;
+       console.warn('21, Ganaste!');
+       turnoComputadora( puntosJugador );
+   }
+});
+btnDetener.addEventListener('click', () => {
+
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    
+    turnoComputadora( puntosJugador );
+});
+// btnDetener.addEventListener('Click', () => {
+//     btnPedir.disabled = true;
+//     btnDetener.disabled = true;
+
+//     turnoComputadora( puntosJugador );
+// });
