c9e265d246044883eec7c88fe0cf6a2bce16263c
Fernando <fernandoalcocer812@hotmail.com>
2022-04-27 12:59:29 -0300

    Proyecto de laboratorio nuevo, completado hasta eliminar un todo

---
+++ 05- ToDo/src/index.js
@@ -0,0 +1,20 @@
+import './styles.css';
+
+import { Todo, TodoList } from "./classes";
+import { crearTodoHtml } from './js/componentes';
+// import { Todo } from './classes/todo.class.js';
+// import { TodoList } from "./classes/todo-list.class";
+
+
+
+export const todoList = new TodoList();
+
+
+const tarea = new Todo('Aprender Javascript!!');
+todoList.nuevoTodo ( tarea );
+
+tarea.completado = true;
+
+console.log( todoList);
+
+crearTodoHtml( tarea );
