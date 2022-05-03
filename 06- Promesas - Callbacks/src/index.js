d26950e4df263d58c88ffb07064e82c0b2d9a17b
Fernando <fernandoalcocer812@hotmail.com>
2022-05-03 18:24:13 -0300

    Progreso en promesas y callbacks

--- 06- Promesas - Callbacks/src/index.js
+++ 06- Promesas - Callbacks/src/index.js
@@ -1,14 +1,1 @@
 import './styles.css';
-import { buscarHeroe } from './js/callbacks';
-
-const heroeId = 'capi';
-
-
-buscarHeroe( heroeId, ( err, heroe ) => {
-   
-   if ( err ) {
-    console.error( err );
-   } else {
-       console.log( heroe );
-   }
-});
---
+++ 06- Promesas - Callbacks/src/js/uso-call-prom.back.js
@@ -0,0 +1,42 @@
+import './styles.css';
+import { buscarHeroe as buscarHeroeCallback } from './js/callbacks';
+import { buscarHeroe } from './js/promesas';
+
+const heroeId1 = 'capi';
+const heroeId2 = 'spider';
+
+
+// buscarHeroe( heroeId1, ( err, heroe1 ) => {
+   
+//    if ( err ) { return console.error( err ); } 
+
+
+//     buscarHeroe( heroeId2, ( err, heroe2 ) => {
+
+//         if ( err ) { return console.error( err ); }
+
+//         console.log(`Enviando a ${ heroe1.nombre } y ${heroe2.nombre} a la mision`);
+//     });
+
+// });
+
+
+// buscarHeroe( heroeId1 ).then( heroe => {
+
+//     buscarHeroe( heroreId2 ).then( heroe2 => {
+//         console.log(`Enviando a ${ heroe1.nombre} y ${ heroe2.nombre } a la mision`)
+//     });
+
+// });
+
+Promise.all([ buscarHeroe(heroeId1), buscarHeroe(heroeId2) ])
+       .then( ([heroe1, heroe2]) => {
+
+        console.log(`Enviando a ${ heroe1.nombre } y ${ heroe2.nombre } a la mision`);
+       }).catch( err => {
+           alert( err )
+       }).finally( () => {
+           console.log('Se termino el promise.all');
+       })
+
+console.log('Fin de programa');
