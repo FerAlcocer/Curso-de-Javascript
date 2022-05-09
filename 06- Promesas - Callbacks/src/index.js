154bb2615ad73a4a564ee8b04f3e936281e0c2a0
Fernando <fernandoalcocer812@hotmail.com>
2022-05-09 15:31:49 -0300

    Se finalizo callbacks y promesas

--- 06- Promesas - Callbacks/src/index.js
+++ 06- Promesas - Callbacks/src/index.js
@@ -1,1 +1,37 @@
-import './styles.css';
+// // import { buscarHeroe, buscarHeroeAsync } from "./js/promesas";
+// // // import { promesaLenta, promesaMedia, promesaRapida } from "./js/promesas";
+
+
+// // // promesaLenta.then( console.log );
+// // // promesaMedia.then( console.log );
+// // // promesaRapida.then( console.log );
+
+// // // Promise.race([ promesaLenta, promesaMedia, promesaRapida ])
+// // //         .then( console.log )
+// // //         .catch( console.log );
+
+// // buscarHeroe( 'capi2' )
+// //         .then( heroe => console.log(heroe) )
+// //         .catch( console.warn ); 
+
+// // buscarHeroeAsync( 'iron2' )
+// //         .then( heroe => console.log(heroe) )
+// //         .catch( console.warn ); 
+
+// import { obtenerHeroesArr, obtenerHeroeAwait } from "./js/await";
+
+// console.time('await');
+
+// obtenerHeroeAwait('capi2')
+//     .then( heroe =>{
+//     console.log( heroe );
+
+//     console.timeEnd('await');
+
+// }).catch( console.warn );
+
+import { heroesCiclo, heroeIfAwait } from './js/await';
+
+heroesCiclo();
+
+heroeIfAwait('iron');
--- 06- Promesas - Callbacks/src/js/promesas.js
+++ 06- Promesas - Callbacks/src/js/promesas.js
@@ -20,11 +20,22 @@ export const buscarHeroe = ( id ) => {
     return new Promise( ( resolve, reject ) => {
 
         if ( heroe ) {
-            resolve ( heroe );
+            setTimeout(() => resolve( heroe ), 1000);
         } else {
             reject(`No existe un heroe con el id ${ id }`);
         }
     });
+}
+
+export const buscarHeroeAsync = async( id ) => {
+
+    const heroe = heroes [id];
+
+        if ( heroe ) {
+            return heroe ;
+        } else {
+            throw`No existe un heroe con el id ${ id }`;
+        }
 }
 
 const promesaLenta = new Promise( ( resolve, reject ) => {
@@ -38,3 +49,9 @@ const promesaMedia = new Promise( ( resolve, reject ) => {
 const promesaRapida = new Promise( ( resolve, reject ) => {
     setTimeout(() => resolve('Promesa Rapida'), 1000);
 });
+
+export {
+    promesaLenta,
+    promesaMedia,
+    promesaRapida
+};
